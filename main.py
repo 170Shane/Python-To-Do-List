c137aeffb31fcbb0b497780efe8268640f4072ff
Shane Clark <170shane@gmail.com>
2025-03-21 12:49:15 +0000

    Enhance user action input handling by adding lowercase conversion and provide match statement implementation as a comment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ my_todo_list = []
 
 while True:
     user_action = input("Type 'add' or 'show' or 'exit': ") # Get user input
-    user_action = user_action.strip().lower() # Remove leading and trailing whitespaces
+    user_action = user_action.strip().lower() # Remove leading and trailing whitespaces and convert to lowercase
 
     if user_action == "add": # If the user types "add"
         user_input = input("Type an item to add to the To-Do list: ") # Get user input
@@ -15,4 +15,19 @@ while True:
         break
     else:
         print("Invalid action. Please type 'add' or 'show' or 'exit'") # Print an error message
+        
+        
+        
+# This can also be implemented using a match statement in Python 3.10+:
+# match user_action:
+#     case "add":
+#         user_input = input("Type an item to add to the To-Do list: ")
+#         my_todo_list.append(user_input)
+#     case "show":
+#         print(" ".join(my_todo_list))
+#     case "exit":
+#         print("Exiting...Bye!")
+#         break
+#     case _:
+#         print("Invalid action. Please type 'add' or 'show' or 'exit'")
         
