459002759c001c7f1d476fa7bee5419dc9b46d40
shaneclark <170shane@gmail.com>
2025-03-21 11:04:57 +0000

    Refactor todo list input handling and display, add input sanitization and exit message

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-
-
 my_todo_list = []
+
 
 while True:
     user_action = input("Type 'add' or 'show' or 'exit': ") # Get user input
+    user_action = user_action.strip().lower() # Remove leading and trailing whitespaces
+
     if user_action == "add": # If the user types "add"
-        user_input = input("Type an item to add to the To-Do list': ") # Get user input
+        user_input = input("Type an item to add to the To-Do list: ") # Get user input
         my_todo_list.append(user_input) # Add the user input to the list
     elif user_action == "show":
-        print("To-Do list: ", my_todo_list) # Print the list
+        print(" ".join(my_todo_list))
     elif user_action == "exit": # If the user types "exit"
+        print("Exiting...Bye!")
         break
     else:
         print("Invalid action. Please type 'add' or 'show' or 'exit'") # Print an error message
         
-
-# while True:
-#     user_input = input("Enter an item to add to the To-Do list: ") # Get user input
-#     if user_input == "exit": # If the user types "exit"
-#         break # Exit the loop  
-#     my_todo_list.append(user_input) # Add the user input to the list
-#     print("To-Do list: ", my_todo_list) # Print the list
-    
