c5f1a03aa834fc85d8625884363d5d09423ba3f8
shaneclark <170shane@gmail.com>
2025-03-25 16:45:47 +0000

    Add sorting functionality for waiting list and display indexed names

---
+++ bonus/sorting.py
@@ -0,0 +1,6 @@
+waiting_list = ["Shane","Callum","Julie","Sally","Peter","John","Mary","Paul","Emma","David"]
+waiting_list.sort()
+
+for index, name in enumerate(waiting_list):
+    print(f"{index+1}. {name}")
+
