d8fbb4ab8ac253b70b627a2e2374126798b73d44
Shane Clark <170shane@gmail.com>
2025-04-21 11:03:52 +0100

    Add tooltip to 'Add' button in GUI for better user guidance

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ import FreeSimpleGUI as fsg
 
 label_text = FreeSimpleGUI.Text("Add an item to the To-Do List:") # Create a label for the To-Do list
 input_box = FreeSimpleGUI.InputText(tooltip="Enter a To-Do item") # Create an input box for the To-Do item
-add_button = FreeSimpleGUI.Button("Add") # Create a button to add the To-Do item
+add_button = FreeSimpleGUI.Button("Add", tooltip="Add the To-Do item to the list") # Create a button to add the To-Do item
 
 window = fsg.Window(title="To-Do List", layout=[[label_text], [input_box, add_button]])
 window.read() # Read the window
