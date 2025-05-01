bb2a51cc4df3a18e94f951fbdca1c6ac939ca9da
shaneclark <170shane@gmail.com>
2025-05-01 06:19:44 +0100

    Fix formatting by adding newline at the end of the file and ensuring proper output of countries list

--- bonus/bonus4.py
+++ bonus/bonus4.py
@@ -6,3 +6,5 @@ while True: # Infinite loop
     country = input("Enter a country: ") # Get user input
     countries.append(country) # Add the user input to the list
     print(countries) # Print the list
+
+print("Countries: ", countries) # Print the list
