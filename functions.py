4841038d45b1a97d415f1fae7aa8aaaaf984fc78
Shane Clark <170shane@gmail.com>
2025-05-30 07:59:07 +0100

    Refactor To-Do list functionality: update todos, enhance GUI interaction, and implement file handling

--- functions.py
+++ functions.py
@@ -41,3 +41,17 @@ if __name__ == "__main__":
     print("The `if __name__ == '__main__':` block is a special construct in Python that allows the file to be imported as a module without running the code inside this block.")
     print("The `exit()` function is called to terminate the program immediately, so no code below this line is executed.")
     print("Please run the main.py file instead.")
+    
+    
+def get_todos(filepath=FILEPATH):
+    """Get the To-Do list from the file.
+
+    Args:
+        filepath (str, optional): The path to the file. Defaults to "files/todos.txt".
+
+    Returns:
+        list: The To-Do list.
+    """
+    with open(filepath, "r") as file:        # Open the file in read mode    
+        my_todo_list = file.readlines() # Read the lines from the file and assign them to the list
+    return my_todo_list
--- gui.py
+++ gui.py
@@ -3,10 +3,24 @@ import FreeSimpleGUI
 import FreeSimpleGUI as fsg
 
 label_text = FreeSimpleGUI.Text("Add an item to the To-Do List:") # Create a label for the To-Do list
-input_box = FreeSimpleGUI.InputText(tooltip="Enter a To-Do item") # Create an input box for the To-Do item
+input_box = FreeSimpleGUI.InputText(tooltip="Enter a To-Do item", key="todo") # Create an input box for the To-Do item
 add_button = FreeSimpleGUI.Button("Add", tooltip="Add the To-Do item to the list") # Create a button to add the To-Do item
 
-window = fsg.Window(title="To-Do List", layout=[[label_text], [input_box, add_button]])
-window.read() # Read the window
-window.close() # Close the window
+
+window = fsg.Window(title="To-Do List", 
+                    layout=[[label_text], [input_box, add_button]], 
+                    font=("Helvetica", 20))# Create a window with the label, input box, and button      
+
+while True:  # Start an infinite loop to keep the window open
+    event, values = window.read()  # Read the window and get the event and input values
+
+    match event : 
+        case "Add":  # If the 'Add' button is clicked
+            existing_todos = functions.get_todos()  # Call the function to get the current To-Do list
+            existing_todos.append(values["todo"] + "\n")  # Append the new To-Do item to the list
+            functions.write_todos(my_todo_list=existing_todos)  # Write the new To-Do item to the file
+            functions.show_todos()  # Show the updated To-Do list
+        case fsg.WIN_CLOSED:  # If the window is closed
+            break  # Exit the loop and close the window
+
 
